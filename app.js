58a4bac59720373ae817bb45e6dd0f122b37c44d
jessevandenkerckhove <jesse.vandenkerckhove@be-mobile.com>
2023-04-02 00:27:49 +0200

    Adding basic functionality for adventures

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var storiesRouter = require('./routes/stories');
+var adventuresRouter = require('./routes/adventures')
 
 
 var app = express();
@@ -26,6 +27,7 @@ app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/stories', storiesRouter);
+app.use('/adventures', adventuresRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
